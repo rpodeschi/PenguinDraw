ae911363c65ae69bd937540c33d9363b32f7d7c3
rpodeschi <rpodeschi@LEN-T440-GTUX.at.millikin.edu>
2017-10-27 15:25:33 -0500

    27-OCT-17, RJ: Add helper methods to return day of registration bib numbers and total prizes.

--- src/RandomWinners.java
+++ src/RandomWinners.java
@@ -8,13 +8,18 @@
  *
  * Audit Trail: 15-MAR-16, RJ: Remove comments and clean up code.
  *                             Add additional array.sort before printing.
- *
+ * Audit Trail: 27-OCT-17, RJ: Resolve duplicate number issue.
+ *                               Use Random.ints Stream to pull distinct numbers.
+ *                             Add day of registration winners.
+ *                             Write winners to winners.csv
  */
 
 // Values used for 2014: Prizes 49, start 7925, end 8905
 // Values used for 2016:
 
 import java.util.*;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
 
 public class RandomWinners
 {
@@ -24,36 +29,47 @@ public class RandomWinners
         header.printHeader();
 
         Scanner keyboard = new Scanner(System.in);
+        Random random = new Random();
 
         Prizes award = new Prizes();
         award.inputPrizeValues();
-
+        
         System.out.println("Prize Table\nWinning Numbers");
         System.out.println("----------------------------");
 
-        int[] firstArray;
-        int totalPrizes = award.getTotalPrizes();
-        int startBibNo = award.getStartBibNum();
-        int endBibNo = award.getEndBibNum();
-        firstArray = new int[totalPrizes];
+        int[] preRegArray = new Random().ints(award.getPreRegStartBibNum(),award.getPreRegEndBibNum()).distinct().limit(award.getPreRegPrizes()).sorted().toArray();
+        Arrays.sort(preRegArray);
+        int[] dayRegArray = new Random().ints(award.getDayRegStartBibNum(),award.getDayRegEndBibNum()).distinct().limit(award.getDayRegPrizes()).sorted().toArray();
+        Arrays.sort(dayRegArray);
+        
+        // Write the winners to the console and output to file.
+        String userDir = "C:\\Users\\rpodeschi\\Documents\\NetBeansProjects\\PenguinDraw\\data\\";
+        String outputFile = userDir + "winners.csv";
 
-        for (int i = 1; i < totalPrizes; i++) {
-            firstArray[i] = (int) (Math.random() *
-                    (endBibNo - startBibNo)) + (startBibNo - 1);
+        PrintWriter outputStream = null;
+        try
+        {
+            outputStream = new PrintWriter(outputFile);
         }
-        Arrays.sort(firstArray);
-
-        for (int j = 1; j < firstArray.length; j++) {
-            if(firstArray[j] == firstArray[j - 1]) {
-                firstArray[j] = (int) (Math.random() *
-                        (endBibNo - startBibNo)) + (startBibNo - 1);
-            }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("Error opening the file " + outputFile);
+            System.exit(0);
         }
-
-        Arrays.sort(firstArray);
-
-        for (int k = 1; k < firstArray.length; k++) {
-            System.out.println(firstArray[k]);
+               
+        // Print the pre-registration prizes to the console
+        for (int i = 1; i < preRegArray.length; i++) {
+            System.out.println(preRegArray[i]);
+            outputStream.println(preRegArray[i]);
         }
+        
+        
+        for (int j = 1; j < dayRegArray.length; j++) {
+            System.out.println(dayRegArray[j]);
+            outputStream.println(dayRegArray[j]);
+        }
+        
+        outputStream.close();
+        System.out.println("Winning bib numbers were written to " + outputFile);
     }
 }
