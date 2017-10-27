e10de65d74af256cdeb851ce8cb98cdc31228947
rpodeschi <rpodeschi@LEN-T440-GTUX.at.millikin.edu>
2017-10-27 16:35:35 -0500

    27-Oct-2017, RJ: Created GUI splashDialog in place of printHeader, added GPL 2.0 header in all files.

--- src/ProgramInfo.java
+++ src/ProgramInfo.java
@@ -6,7 +6,37 @@
  *
  * Audit Trail: 15-MAR-16, RJ: Code clean up.
  *              27-OCT-17, RJ: Update version information.
- */
+ * 
+ * Copyright (C) 2017 RJ Podeschi
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Millikin University and Penguin in the Park, NFP, hereby disclaims 
+ * all copyright interest in the program Penguin Draw 
+ * (which makes passes at compilers) written by RJ Podeschi.
+ *
+ * signature of RJ Podeschi, October 27, 2017
+ * Millikin University
+ * Penguin in the Park, NFP
+ *
+**/
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+
 public class ProgramInfo {
 
     private final static String PROG_NAME = "Penguin Draw";
@@ -15,14 +45,21 @@ public class ProgramInfo {
     private final static String LAST_MOD_DATE = "27-OCT-2017";
 
     /**
-     * printHeader displays general program information.
+     * splashDialog displays general program information.
      */
-    public void printHeader() {
-        System.out.println(PROG_NAME);
-        System.out.println("Version: " + VERSION);
-        System.out.println("Last Release: " + LAST_MOD_DATE);
-        System.out.println("Author: " + AUTHOR);
-        System.out.println();
-        System.out.println("-------------------------------------------");
+    public void splashDialog() {
+
+        JFrame frame = new JFrame("JOptionPane Program Splash Screen");
+        ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource("img\\penguin32_29.png"));
+
+        JOptionPane.showMessageDialog(frame,
+                PROG_NAME + "\n" +
+                "Version: " + VERSION + "\n" +
+                "Release Date: " + LAST_MOD_DATE + "\n" +
+                "Author: " + AUTHOR + "\n" +
+                "License Information: ",
+                PROG_NAME,
+                JOptionPane.INFORMATION_MESSAGE,
+                icon);
     }
 }
--- src/RandomWinners.java
+++ src/RandomWinners.java
@@ -12,10 +12,32 @@
  *                               Use Random.ints Stream to pull distinct numbers.
  *                             Add day of registration winners.
  *                             Write winners to winners.csv
+ * 
+ * Copyright (C) 2017 RJ Podeschi
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Millikin University and Penguin in the Park, NFP, hereby disclaims 
+ * all copyright interest in the program Penguin Draw 
+ * (which makes passes at compilers) written by RJ Podeschi.
+ *
+ * signature of RJ Podeschi, October 27, 2017
+ * Millikin University
+ * Penguin in the Park, NFP
+ *
  */
-
-// Values used for 2014: Prizes 49, start 7925, end 8905
-// Values used for 2016:
 
 import java.util.*;
 import java.io.PrintWriter;
@@ -26,7 +48,7 @@ public class RandomWinners
     public static void main(String[] args) {
 
         ProgramInfo header = new ProgramInfo();
-        header.printHeader();
+        header.splashDialog();
 
         Scanner keyboard = new Scanner(System.in);
         Random random = new Random();
